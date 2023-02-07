79e667f112f0cd013c5007e7492d37febabd6cbb
Binod nayak <64028114+BINODNAYAK806@users.noreply.github.com>
2023-02-08 01:40:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+import TodoList from './comp/Todo';
+
+
+function App() {
+  return (
+
+    <TodoList/>
+
+    );
+}
+
+export default App;
